5d20a06d2e81cea8364f82acf441c1a45264c405
pawan227 <pawan227@gmail.com>
2016-04-13 17:45:49 -0700

    Chrome Version

---
+++ SeleniumWebDriverAutomation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace SeleniumWebDriverAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                string path = @"C:\chromedriver_win32";
+
+                
+                IWebDriver driver = new ChromeDriver(path);
+                driver.Navigate().GoToUrl("http://gateway.wipro.com");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
